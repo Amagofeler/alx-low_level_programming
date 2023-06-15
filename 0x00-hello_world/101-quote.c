ea125f90d0d83699944130695ad0d5543297b14a
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-15 16:44:22 -0700

    Quote addition

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '#include <unistd.h>' > tmp.c; echo 'int main(void) { return (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) != 59); }' >> tmp.c; gcc tmp.c -o tmp; ./tmp; rm tmp tmp.c
+echo #include <unistd.h>\nint main(void) { write(2, "and that piece of art is useful\\" - Dora Korpar, 2015-10-19\\n", 59); return 1; }' > quote.c && gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote quote.c
