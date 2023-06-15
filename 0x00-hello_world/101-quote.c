059f8130173bac5b6d6f4bbfd50c4535d6c933cd
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-15 16:38:09 -0700

    Another quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,2 @@
-#include <unistd.h>
-int main(void) { return (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) != 59); }
+#!/bin/bash
+echo '#include <unistd.h>' > tmp.c; echo 'int main(void) { return (write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59) != 59); }' >> tmp.c; gcc tmp.c -o tmp; ./tmp; rm tmp tmp.c
