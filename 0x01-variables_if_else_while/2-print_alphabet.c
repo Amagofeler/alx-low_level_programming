ee26df8948a240583c5866417dad902976d3919d
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-16 14:08:43 -0700

    Two print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+Here is one possible solution for your task:
+
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+
+    c = 'a';
+    while (c <= 'z')
+    {
+        putchar(c);
+        c++;
+    }
+    putchar('\n');
+    return (0);
+}
