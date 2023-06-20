886d8ac5d78dcdcb7d6e2d757442e64f494577b7
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-20 14:50:28 -0700

    Difficult Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,18 @@
-#include "_putchar.h"
+#include "main.h"
 
+/**
+ * main - prints _putchar. followed by another line
+ * Return: 0 if Successful
+ */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
+	char s[] = "_putchar\n";
+    	int i = 0;
 
-    return 0;
+	while (s[i])
+	{
+		_putchar(s[i])
+		i++;
+	]
+    	return 0;
 }
