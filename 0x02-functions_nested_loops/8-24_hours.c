77ff06093abf195933167517e52e5a0d09c49432
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-21 16:03:23 -0700

    Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,13 @@
  */
 void jack_bauer(void)
 {
-    int hours, minutes;
+	int hours, minutes;
 
-    for (hours = 0; hours < 24; hours++)
-    {
-        for (minutes = 0; minutes < 60; minutes++)
-        {
-            printf("%02d:%02d\n", hours, minutes);
-        }
-    }
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			printf("%02d:%02d\n", hours, minutes);
+		}
+	}
 }
