0ae67e5ac15065eaf31c8b32230a64dbcbf3a180
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-21 16:18:06 -0700

    Timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,18 +6,18 @@
  */
 void times_table(void)
 {
-    int i, j, result;
+	int i, j, result;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            if (j == 0)
-                printf("%d", result);
-            else
-                printf(",  %2d", result);
-        }
-        printf("\n");
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				printf("%d", result);
+			else
+				printf(",  %2d", result);
+		}
+		printf("\n");
+	}
 }
