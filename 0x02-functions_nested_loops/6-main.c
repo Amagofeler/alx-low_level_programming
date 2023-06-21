eca69c86763b59c68acc186be859fe1500e36423
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-21 14:58:12 -0700

    Permission denied

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -16,15 +15,15 @@ int _abs(int n);
  */
 int main(void)
 {
-	int r;
+    int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
 }
