d824ed2930525f80112e9f3f6c8acd7eedc7e2f4
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-21 14:03:03 -0700

    This is lower

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,20 +1,30 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * test_islower - Tests the _islower function.
+ * @n: The character to test.
  *
  * Return: Always 0.
  */
-int main(void)
+int test_islower(int n)
 {
     int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
+    r = _islower(n);
     _putchar(r + '0');
     _putchar('\n');
+    return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    test_islower('H');
+    test_islower('o');
+    test_islower(108);
     return (0);
 }
