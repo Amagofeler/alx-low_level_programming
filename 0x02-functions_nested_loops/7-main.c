ce051c6a0c8a9b5d03f905021be70875f4d30fb1
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-21 15:40:21 -0700

    Last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,25 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#include <limits.h>
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
     int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
     r = print_last_digit(-1024);
     _putchar('0' + r);
     _putchar('\n');
-
-    r = print_last_digit(INT_MIN);
-    _putchar('0' + r);
-    _putchar('\n');
-
     return (0);
 }
