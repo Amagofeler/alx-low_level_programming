08139cac38ae171aa505d1cc448db123685b0da8
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-06-26 16:14:14 -0700

    Congrats

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,35 +6,19 @@
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];  // +1 for the null terminator
+    char password[PASSWORD_LENGTH + 1]; /* +1 for the null terminator */
     int i;
 
-    srand(time(NULL));  // Seed the random number generator with the current time
+    srand(time(NULL));
 
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
-        int randomCharType = rand() % 3;  // Generate a random number from 0 to 2
-
-        if (randomCharType == 0)
-        {
-            // Generate a random lowercase letter ASCII value (97-122)
-            password[i] = rand() % 26 + 'a';
-        }
-        else if (randomCharType == 1)
-        {
-            // Generate a random uppercase letter ASCII value (65-90)
-            password[i] = rand() % 26 + 'A';
-        }
-        else
-        {
-            // Generate a random digit ASCII value (48-57)
-            password[i] = rand() % 10 + '0';
-        }
+        password[i] = rand() % 94 + 33; /* Generate random printable ASCII characters */
     }
 
-    password[PASSWORD_LENGTH] = '\0';  // Add null terminator to the end of the password
+    password[PASSWORD_LENGTH] = '\0'; /* Add the null terminator */
 
-    printf("%s\n", password);
+    printf("%s", password);
 
     return 0;
 }
