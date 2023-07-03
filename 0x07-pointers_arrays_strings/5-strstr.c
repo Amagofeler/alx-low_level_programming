b6dc57ce3ce76407399efb0c2d7287fdd2fbeabf
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-03 10:27:18 -0700

    World

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,28 +7,28 @@
  * @needle: the substring to find
  *
  * Return: a pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+    int i, j;
 
-	if (*needle == '\0')
-		return haystack;
+    if (*needle == '\0')
+        return haystack;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		if (haystack[i] == needle[0])
-		{
-			for (j = 0; needle[j] != '\0'; j++)
-			{
-				if (haystack[i + j] != needle[j])
-					break;
-			}
-			if (needle[j] == '\0')
-				return (haystack + i);
-		}
-	}
+    for (i = 0; haystack[i] != '\0'; i++)
+    {
+        if (haystack[i] == needle[0])
+        {
+            for (j = 0; needle[j] != '\0'; j++)
+            {
+                if (haystack[i + j] != needle[j])
+                    break;
+            }
+            if (needle[j] == '\0')
+                return (haystack + i);
+        }
+    }
 
-	return NULL;
+    return (NULL);
 }
