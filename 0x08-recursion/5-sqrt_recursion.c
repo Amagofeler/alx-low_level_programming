c2fdbdf75b3a29b1118e958f0971989e9697622a
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-05 16:25:35 -0700

    Square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,11 @@
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0 || n == 1)
-        return (n);
-    return (_sqrt_helper(n, 1));
+if (n < 0)
+return	(-1);
+if (n == 0 || n == 1)
+return	(n);
+return (_sqrt_helper(n, 1));
 }
 
 /**
@@ -26,9 +26,9 @@ int _sqrt_recursion(int n)
  */
 int _sqrt_helper(int n, int i)
 {
-    if (i * i > n)
-        return (-1);
-    if (i * i == n)
-        return (i);
-    return (_sqrt_helper(n, i + 1));
+if (i * i > n)
+return	(-1);
+if (i * i == n)
+return	(i);
+return (_sqrt_helper(n, i + 1));
 }
