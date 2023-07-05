79400a9c07fe7c98b7788e13bb9e9540f6a586c5
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-05 15:04:23 -0700

    Putting up with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n'); /* Print a new line at the end of the string */
-        return;
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n'); /* Print a new line at the end of the string */
+		return;
+	}
 
-    _putchar(*s); /* Print the current character */
-    _puts_recursion(s + 1); /* Recursively call the function with the next character */
+	_putchar(*s); /* Print the current character */
+	_puts_recursion(s + 1); /* Recursively call the function with the next character */
 }
