ea9351470b0a7d5d2cc37fe4db2f90417e990436
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-05 15:16:39 -0700

    Putting up with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
+ * _puts_recursion - prints a string followed by a new line using recursion
+ * @s: string to print
+ *
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n'); /* Print a new line at the end of the string */
-		return;
-	}
-
-	_putchar(*s); /* Print the current character */
-	_puts_recursion(s + 1); /* Recursively call the function
-	                          with the next character */
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
+    _putchar(*s);
+    _puts_recursion(s + 1);
 }
