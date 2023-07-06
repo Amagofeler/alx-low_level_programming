d828c38d4b8f87cece7b9e3596f107ca68ce007e
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-05 17:22:01 -0700

    Centuries of palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,45 @@
 #include "main.h"
 
 /**
- * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
- * @s: string to check if it's a palindrome.
+ * is_palindrome - Returns 1 if a string is a palindrome and 0 if not.
+ * @s: String to check if it's a palindrome.
  *
  * Return: 1 if s is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
-    int len = _strlen_recursion(s);
+int len = _strlen_recursion(s);
 
-    if (len <= 1)
-        return (1);
-    return (_palindrome_helper(s, len - 1));
+if (len <= 1)
+return  (1);
+return (_palindrome_helper(s, len));
 }
 
 /**
- * _palindrome_helper - helper function for is_palindrome.
- * @s: string to check if it's a palindrome.
- * @len: length of s.
+ * _palindrome_helper - Helper function for is_palindrome.
+ * @s: String to check if it's a palindrome.
+ * @len: Length of s.
  *
  * Return: 1 if s is a palindrome, 0 otherwise.
  */
 int _palindrome_helper(char *s, int len)
 {
-    if (*s != *(s + len))
-        return (0);
-    if (len <= 1)
-        return (1);
-    return (_palindrome_helper(s + 1, len - 2));
+if (*s != *(s + len - 1))
+return  (0);
+if (len <= 1)
+return  (1);
+return (_palindrome_helper(s + 1, len - 2));
 }
 
 /**
- * _strlen_recursion - returns the length of a string.
- * @s: string to calculate the length of.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: String to calculate the length of.
  *
- * Return: the length of s.
+ * Return: The length of s.
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+return  (0);
+return (1 + _strlen_recursion(s + 1));
 }
