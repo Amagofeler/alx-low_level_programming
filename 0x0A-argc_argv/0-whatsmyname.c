086e940c82ee867a242c7bafa22aeb41c8301e0c
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-11 02:32:47 -0700

    My new name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    /* Unused parameter */
-    (void)argc;
+int main(int argc, char *argv[])
+{
+/* Unused parameter */
+ (void)argc;
 
-    printf("%s\n", argv[0]);
+ printf("%s\n", argv[0]);
 
-    return 0;
+ return 0;
 }
