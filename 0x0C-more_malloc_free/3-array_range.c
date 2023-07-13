ebf057aa30f131b2d569b541605eb5717286b9eb
Abdulkhafid <auruibrahimabdulkhafid@gmail.com>
2023-07-13 06:58:27 -0700

    Range of arrays

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,24 +12,24 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int size, i;
+int *arr;
+int size, i;
 
     /* Check if min is greater than max */
-    if (min > max)
-        return (NULL);
+if (min > max)
+return (NULL);
 
     /* Calculate the size of the array */
-    size = max - min + 1;
+size = max - min + 1;
 
     /* Allocate memory for the array */
-    arr = malloc(size * sizeof(int));
-    if (arr == NULL)
-        return (NULL);
+arr = malloc(size * sizeof(int));
+if (arr == NULL)
+return (NULL);
 
     /* Fill the array with values from min to max */
-    for (i = 0; i < size; i++)
-        arr[i] = min++;
+for (i = 0; i < size; i++)
+arr[i] = min++;
 
-    return (arr);
+return (arr);
 }
